46dc6c62c487af765842219a660f4d7f1621ace0
Anushree-2005 <157889417+Anushree-2005@users.noreply.github.com>
2024-01-29 16:44:14 +0000

    container config done

---
+++ 27thJan_JS/03_conversionOperation.js
@@ -0,0 +1,63 @@
+let score= "123"
+console.log(typeof score);
+
+//typecasting means converting one datatype into another datatype
+let ValueNumber= Number(score);
+// console.log(typeof ValueNumber);//output is number
+// console.log(ValueNumber);// output is NaN
+
+
+//"22"=>22
+//"123abc"=>NaN
+//true=>1; false=>0
+
+
+let someNumber="Anushree"
+
+let stringNumber=String(someNumber)
+//no point typecasting string var to string var again
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+/*      oprations       */
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+// let str1="Anushree"
+// let str2="chavan"
+
+// let fullName=str1+" "+str2;
+// console.log(fullName);
+
+// console.log("1"+2);
+// console.log("1"+2+2+"5");//1225
+// console.log("1"+(2+2)+"5");//145
+
+// console.log((3+4)*5%3);
+console.log((+true));
+console.log(+"");
+
+let num1,num2,num3;
+
+num1=num2=num3=2+2;
+// console.table([num1,num2,num3]);
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
